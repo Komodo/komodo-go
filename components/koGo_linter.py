e89a1dadf3bf6e7768842d0c99071426782f5fc9
Travis Cline <travis.cline@gmail.com>
2013-05-11 14:55:20 -0500

    Add smarter go binary and GOPATH checking.
    Copy buffer into build area.

--- components/koGo_linter.py
+++ components/koGo_linter.py
@@ -68,23 +68,30 @@ class KoGoLinter(object):
             getService(components.interfaces.koISysUtils)
         self._koDirSvc = components.classes["@activestate.com/koDirs;1"].\
             getService(components.interfaces.koIDirs)
-        self._check_for_go_binary()
 
-    def _check_for_go_binary(self):
-        try:
-            subprocess.call(['go'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        except OSError:
-            log.error('"go" binary not found.')
 
     def lint(self, request):
-        log.info(request)
         encoding_name = request.encoding.python_encoding_name
         text = request.content.encode(encoding_name)
         return self.lint_with_text(request, text)
 
     def lint_with_text(self, request, text):
-        log.info(request)
-        log.debug(text)
+        results = koLintResults()
+        log.debug(request)
+
+        env = koprocessutils.getUserEnv()
+        try:
+            subprocess.call(['go', 'env'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+        except OSError:
+            log.error('"go" binary not found!')
+            results.addResult(_errorResult('"go" binary not found! (PATH=%s)' % env['PATH']))
+            return results
+
+        # ensure GOPATH is present
+        if 'GOPATH' not in env:
+            results.addResult(_errorResult('"GOPATH" not in environment, please check your general or project settings'))
+            return results
+
         if not text.strip():
             return None
         # consider adding lint preferences? maybe for compiler selection, paths, etc?
@@ -102,11 +109,12 @@ class KoGoLinter(object):
 
         shutil.copytree(request.cwd, temp_dir, ignore=non_go_files)
 
+        buf = open(os.path.join(temp_dir, request.koDoc.baseName),'w')
+        buf.write(text)
+        buf.close()
+
         compilation_command = ['go', 'build']
         try:
-            env = koprocessutils.getUserEnv()
-            results = koLintResults()
-
             log.info('Running ' + ' '.join(compilation_command))
             p = process.ProcessOpen(compilation_command, cwd=temp_dir, env=env, stdin=None)
             output, error = p.communicate()
@@ -121,27 +129,34 @@ class KoGoLinter(object):
                 for line in all_output:
                     if line and line[0] == '#':
                         continue
-                    results.addResult(self._buildResult(text, line, request.koDoc.baseName))
+                    results.addResult(_buildResult(text, line, request.koDoc.baseName))
             else:
                     results.addResult(self._buildResult(text, "Unexpected error"))
         return results
 
-    def _buildResult(self, text, message, filename=None):
-        r = KoLintResult()
-        r.severity = r.SEV_ERROR
-        r.description = message
-        
-        m = re.match('./(.+\.go):(\d+): (.*)', message)
-        if m:
-            lint_message_file, line_no, error_message = m.groups()
-            if lint_message_file != filename:
-                r.description = message
-                return r
-            line_no = int(line_no)
-            line_contents = text.splitlines()[int(line_no)-1].rstrip()
-            r.description = error_message
-            r.lineStart = r.lineEnd = line_no
-            r.columnStart = len(line_contents) - len(line_contents.strip()) + 1
-            r.columnEnd = len(line_contents) + 1
+def _errorResult(description):
+    r = KoLintResult()
+    r.severity = r.SEV_ERROR
+    r.description = description
+    return r
 
-        return r
+def _buildResult(text, message, filename=None):
+    r = KoLintResult()
+    r.severity = r.SEV_ERROR
+    r.description = message
+
+    m = re.match('./(.+\.go):(\d+): (.*)', message)
+    if m:
+        lint_message_file, line_no, error_message = m.groups()
+        if lint_message_file != filename:
+            r.description = message
+            return r
+        line_no = int(line_no)
+        log.debug("_buildResult %s %s", line_no, len(text.splitlines()))
+        line_contents = text.splitlines()[int(line_no)-1].rstrip()
+        r.description = error_message
+        r.lineStart = r.lineEnd = line_no
+        r.columnStart = len(line_contents) - len(line_contents.strip()) + 1
+        r.columnEnd = len(line_contents) + 1
+
+    return r
