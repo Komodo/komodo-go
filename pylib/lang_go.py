cd2c33a11b705d6ffd2490475f5d99e76ece8761
Travis Cline <travis.cline@gmail.com>
2013-07-29 11:29:21 -0700

    Add Komodo 8.5 support

--- pylib/lang_go.py
+++ pylib/lang_go.py
@@ -2,10 +2,13 @@
 
 """Go language support for codeintel."""
 
+import os
+import sys
 import json
 import logging
 import process
-import koprocessutils
+import time
+import ciElementTree as ET
 from codeintel2.accessor import AccessorCache
 from codeintel2.citadel import CitadelLangIntel
 from codeintel2.common import Trigger, TRG_FORM_CALLTIP, TRG_FORM_CPLN, CILEDriver, Definition
@@ -140,7 +143,7 @@ class GoLangIntel(CitadelLangIntel,
         cmd = ['godef', '-i=true', '-t=true', '-f=%s' % buf.path, '-o=%s' % trg.pos]
         log.debug("running [%s]", cmd)
         try:
-            p = process.ProcessOpen(cmd, env=koprocessutils.getUserEnv())
+            p = process.ProcessOpen(cmd, env=buf.env.get_all_envvars())
         except OSError, e:
             log.error("Error executing '%s': %s", cmd[0], e)
             return
@@ -182,14 +185,13 @@ class GoLangIntel(CitadelLangIntel,
 
     def invoke_gocode(self, buf, trg, ctlr):
         pos = trg.pos
-
         if trg.type == "call-signature":
             pos = pos - 1
 
         cmd = ['gocode', '-f=json', 'autocomplete', buf.path, '%s' % pos]
         log.debug("running [%s]", cmd)
         try:
-            p = process.ProcessOpen(cmd, env=koprocessutils.getUserEnv())
+            p = process.ProcessOpen(cmd, env=buf.env.get_all_envvars())
         except OSError, e:
             log.error("Error executing '%s': %s", cmd[0], e)
             return
@@ -253,10 +255,61 @@ class GoBuffer(UDLBuffer):
 class GoCILEDriver(CILEDriver):
     lang = lang
 
-    def scan_purelang(self, buf):
-        import cile_go
-        return cile_go.scan_buf(buf)
+    def scan_purelang(self, buf, mtime=None, lang="Go"):
+        """Scan the given GoBuffer return an ElementTree (conforming
+        to the CIX schema) giving a summary of its code elements.
 
+        @param buf {GoBuffer} is the Go buffer to scan
+        @param mtime {int} is a modified time for the file (in seconds since
+            the "epoch"). If it is not specified the _current_ time is used.
+            Note that the default is not to stat() the file and use that
+            because the given content might not reflect the saved file state.
+        """
+        # Dev Notes:
+        # - This stub implementation of the Go CILE return an "empty"
+        #   summary for the given content, i.e. CIX content that says "there
+        #   are no code elements in this Go content".
+        # - Use the following command (in the extension source dir) to
+        #   debug/test your scanner:
+        #       codeintel scan -p -l Go <example-Go-file>
+        #   "codeintel" is a script available in the Komodo SDK.
+        log.info("scan '%s'", buf.path)
+        if mtime is None:
+            mtime = int(time.time())
+
+        # The 'path' attribute must use normalized dir separators.
+        if sys.platform.startswith("win"):
+            path = buf.path.replace('\\', '/')
+        else:
+            path = buf.path
+
+        tree = ET.Element("codeintel", version="2.0",
+                          xmlns="urn:activestate:cix:2.0")
+        file = ET.SubElement(tree, "file", lang=lang, mtime=str(mtime))
+        blob = ET.SubElement(file, "scope", ilk="blob", lang=lang,
+                             name=os.path.basename(path))
+
+        #TODO:
+        # - A 'package' -> 'blob'. Problem is a single go package can be from
+        #   multiple files... so really would want `lib.get_blobs(name)` instead
+        #   of `lib.get_blob(name)` in the codeintel API. How does Ruby deal with
+        #   this? Perl?
+        # - How do the multi-platform stdlib syscall_linux.go et al fit together?
+
+        # Dev Note:
+        # This is where you process the Go content and add CIX elements
+        # to 'blob' as per the CIX schema (cix-2.0.rng). Use the
+        # "buf.accessor" API (see class Accessor in codeintel2.accessor) to
+        # analyze. For example:
+        # - A token stream of the content is available via:
+        #       buf.accessor.gen_tokens()
+        #   Use the "codeintel html -b <example-Go-file>" command as
+        #   a debugging tool.
+        # - "buf.accessor.text" is the whole content of the file. If you have
+        #   a separate tokenizer/scanner tool for Go content, you may
+        #   want to use it.
+
+        return tree
 
 
 
@@ -271,7 +324,5 @@ def register(mgr):
         langintel_class=GoLangIntel,
         import_handler_class=None,
         cile_driver_class=GoCILEDriver,
-        # Dev Note: set to false if this language does not support
-        # autocomplete/calltips.
         is_cpln_lang=True)
 
